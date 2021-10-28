925db5a6c0e79057291851320665b9ce163e145c
MSajedian <mohammadsajedian@gmail.com>
2021-10-28 02:09:22 +0100

    eyes added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 export default function Home() {
+    const [pupilTop, setPupilTop] = useState("50%");
+    const [pupilLeft, setPupilLeft] = useState("50%");
+    const [pupilTransform, setPupilTransform] = useState("translate(-50%, -50%)");
+
+    function handleMouseMove(event) {
+        let x = (event.clientX * 100) / window.innerWidth + "%";
+        let y = (event.clientY * 100) / window.innerHeight + "%";
+        setPupilLeft(x);
+        setPupilTop(y);
+        setPupilTransform(`translate(-${x},-${y})`);
+    }
+
     return (
-        <Container>
+        <Container className="home-container" onMouseMove={(event) => handleMouseMove(event)}>
             <h2>Home</h2>
             <Row>
-                <Col>
+                <Col lg={6}>
                     <p>contact icons in Each page</p>
 
                     <p>Images:</p>
@@ -19,9 +31,32 @@ export default function Home() {
 
                     <p>“DO WHAT YOU LOVE LOVE WHAT YOU DO” / “EVER POSITIVE NEVER NEGATIVE”</p>
                 </Col>
-                <Col>
-                    <div className="text-center">
+                <Col lg={6}>
+                    <div className="text-center image-container">
                         <img src="../assets/images/hi.png" alt="hi" className="home-image-1" />
+                        <div className="eyes">
+                            <div className="eye">
+                                <div
+                                    className="pupil"
+                                    style={{
+                                        top: pupilTop,
+                                        left: pupilLeft,
+                                        transform: pupilTransform
+                                    }}
+                                ></div>
+                            </div>
+
+                            <div className="eye">
+                                <div
+                                    className="pupil"
+                                    style={{
+                                        top: pupilTop,
+                                        left: pupilLeft,
+                                        transform: pupilTransform
+                                    }}
+                                ></div>
+                            </div>
+                        </div>
                     </div>
                 </Col>
             </Row>
