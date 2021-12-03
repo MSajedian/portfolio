9eb7afa600f2dc2a765b836ff92663227b1c85aa
MSajedian <mohammadsajedian@gmail.com>
2021-12-03 14:54:30 +0000

    description paragraph added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,17 +13,18 @@ export default function Projects() {
                         <Card.Img variant="top" src="../assets/images/topedu.jpg" />
                         <Card.Body>
                             <Card.Title>TopEdu</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Learning Management System</Card.Subtitle>
                             <div><b><HiOutlineExternalLink className="mb-1" /> Demo:</b>{' '}
                                 <a href="https://topedu.vercel.app/" target="_blank" rel="noreferrer">TopEdu</a>
-                            </div>
-                            <div><b><BsInfoCircle className="mb-1"/> About the TopEdu:</b>{' '}
-                                <a href="https://www.youtube.com/watch?v=i4ehaW-nXZI&list=PLIz8i-HCdhvkycvUn6IYZnCMiM1C3wMPS&index=18" target="_blank" rel="noreferrer">Video</a>
                             </div>
                             <div><b><HiCode className="mb-1"/> Codes:</b>{' '}
                                 <a href="https://github.com/MSajedian/Topedu-Reactjs" target="_blank" rel="noreferrer">Topedu-Reactjs</a> / <a href="https://github.com/MSajedian/Topedu-Expressjs" target="_blank" rel="noreferrer">Topedu-Expressjs</a>
                             </div>
                             <div><b><IoIosApps className="mb-1"/> Tech:</b> ReactJS, Redux, CSS, ExpressJS, NodeJS, MongoDB</div>
+                            <div><b><BsInfoCircle className="mb-1"/> About:</b>{' '}
+                                <a href="https://www.youtube.com/watch?v=i4ehaW-nXZI&list=PLIz8i-HCdhvkycvUn6IYZnCMiM1C3wMPS&index=18" target="_blank" rel="noreferrer">Video</a>
+                            </div>
+                        <hr />
+                        <div className="description-par-1">TopEdu is a learning managment system, Teachers can create their courses and students can use these courses. There is a code playground available to all users so they can exectue their html, css and javaScript codes in the app.</div>
                         </Card.Body>
                     </Card>
                 </Col>
